037e539fadd895285c288053b450f9db34f73121
unknown <ss2001shershah@gmail.com>
2025-01-30 20:29:28 +0530

    error is appearing

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+LOGIN_REDIRECT_URL ='food:index'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- users/views.py
+++ users/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 # Create your views here.
 
 def register(request):
-    if request.method=='POST':
-        form = UserCreationForm(request.POST)
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
         if form.is_valid():
+            user = form.save()  # Save the user to the database
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Welcome {username}, your account is created successfully!')
-            return redirect('food:index')
+            messages.success(request, f'Welcome {username}, your account is created successfully!')
+            return redirect('users:login')  # Redirect after successful registration
+        else:
+            # Add a message for invalid form
+            messages.error(request, 'Registration failed. Please check the errors below.')
     else:
-      form = UserCreationForm()
-    return render(request,'users/register.html',{'form':form})
+        form = RegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
